943110a275f3f5635ec4f62dfcb9df72ff22862d
Aaron Simmons <paleozogt@gmail.com>
2018-08-27 15:28:02 -0600

    setup_msvc saves VCVARS32/VCVARS64 env vars

--- vagranttools/setup_msvc.ps1
+++ vagranttools/setup_msvc.ps1
@@ -11,16 +11,21 @@ If ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
 If ($msvc_ver -eq "12") {
     # sadly no "build tools" for 2013
     choco install -y visualstudioexpress2013windowsdesktop vs2013.4
+
+    $vcvarsbat="$programFilesX86\Microsoft Visual Studio 12.0\VC\vcvarsall.bat"
+    $vcvars32="`"$vcvarsbat`" x86"
+    $vcvars64="`"$vcvarsbat`" x86_amd64"
 } ElseIf ($msvc_ver -eq "14") {
     choco install -y visualcpp-build-tools --version 14.0.25420.1
+
+    $vcvarsbat="$programFilesX86\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+    $vcvars32="`"$vcvarsbat`" x86"
+    $vcvars64="`"$vcvarsbat`" amd64"
 } ElseIf ($msvc_ver -eq "15") {
     choco install -y visualcpp-build-tools --version 15.0.26228.20170424
 
-    If ($env:PROCESSOR_ARCHITECTURE -eq "AMD64") {
-        $vcvarsbat="$programFilesX86\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-    } Else {
-        $vcvarsbat="$programFilesX86\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat"
-    }
+    $vcvars32="`"$programFilesX86\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars32.bat`""
+    $vcvars64="`"$programFilesX86\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat`""
 
     # There's a bug in this version of the msvc installer where choco will exit before the installer is done.
     # We have to some workarounds to wait until everything is done.
@@ -35,3 +40,16 @@ If ($msvc_ver -eq "12") {
     echo "waiting for installer to finish"
     Wait-Process -Name vs_installer
 }
+
+choco install -y vswhere
+
+echo "vcvars32 $vcvars32"
+ls $vcvars32
+
+echo "vcvars64 $vcvars64"
+ls $vcvars64
+
+# env vars to easily invoke vcvars
+[Environment]::SetEnvironmentVariable("VCVARS32", $vcvars32, "Machine")
+[Environment]::SetEnvironmentVariable("VCVARS64", $vcvars64, "Machine")
+refreshenv
