f99ad9f0521462f83dd52fbd130b2ace23fb11c5
Aaron Simmons <paleozogt@gmail.com>
2018-09-02 13:35:04 -0600

    changing compare-snapshots ordering

--- vagranttools/compare-snapshots.bat
+++ vagranttools/compare-snapshots.bat
@@ -8,11 +8,11 @@ set OUTPUT=%3
 
 mkdir %OUTPUT%
 
+diff --unchanged-group-format=  %SNAPSHOT1%\files-sorted.txt %SNAPSHOT2%\files-sorted.txt > %OUTPUT%\files.txt
+zip %OUTPUT%\files.zip -@ < %OUTPUT%\files.txt
+
 regdiff %SNAPSHOT1%\HKLM.reg %SNAPSHOT2%\HKLM.reg /DIFF %OUTPUT%\HKLM.reg
 regdiff %SNAPSHOT1%\HKCU.reg %SNAPSHOT2%\HKCU.reg /DIFF %OUTPUT%\HKCU.reg
 regdiff %SNAPSHOT1%\HKCR.reg %SNAPSHOT2%\HKCR.reg /DIFF %OUTPUT%\HKCR.reg
 regdiff %SNAPSHOT1%\HKU.reg %SNAPSHOT2%\HKU.reg /DIFF %OUTPUT%\HKU.reg
 regdiff %SNAPSHOT1%\HKCC.reg %SNAPSHOT2%\HKCC.reg /DIFF %OUTPUT%\HKCC.reg
-
-diff --unchanged-group-format=  %SNAPSHOT1%\files-sorted.txt %SNAPSHOT2%\files-sorted.txt > %OUTPUT%\files.txt
-zip %OUTPUT%\files.zip -@ < %OUTPUT%\files.txt
