bc8d1e606f6467e19ffc01967c1bfaf7df8ccef9
Aaron Simmons <paleozogt@gmail.com>
2018-09-05 14:36:03 -0600

    logging in snapshot.bat

--- vagranttools/snapshot.bat
+++ vagranttools/snapshot.bat
@@ -8,6 +8,7 @@ mkdir %SNAPSHOT_DIR%
 
 set > %SNAPSHOT_DIR%\env.txt
 
+@echo exporting registry
 reg export HKLM %SNAPSHOT_DIR%\HKLM.reg /y
 reg export HKCU %SNAPSHOT_DIR%\HKCU.reg /y
 reg export HKCR %SNAPSHOT_DIR%\HKCR.reg /y
@@ -16,18 +17,23 @@ reg export HKCC %SNAPSHOT_DIR%\HKCC.reg /y
 
 rm -rf %SNAPSHOT_DIR%\files.txt
 
+@echo listing %SystemRoot%
 cd %SystemRoot%
 fd -a -t f >> %SNAPSHOT_DIR%\files.txt
 
+@echo listing %ProgramData%
 cd %ProgramData%
 fd -a -t f >> %SNAPSHOT_DIR%\files.txt
 
+@echo listing %ProgramFiles%
 cd %ProgramFiles%
 fd -a -t f >> %SNAPSHOT_DIR%\files.txt
 
+@echo listing %ProgramFiles(x86)%
 cd %ProgramFiles(x86)%
 fd -a -t f >> %SNAPSHOT_DIR%\files.txt
 
+@echo sorting file list
 sort %SNAPSHOT_DIR%\files.txt > %SNAPSHOT_DIR%\files-sorted.txt
 
 cd %CURR_DIR%
